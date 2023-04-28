a0fd3bcc5a6a565c91166b6d9c1f597d94ce45d0
Jaka Vrhovnik <jaka.vrhovnik2@gmail.com>
2023-04-28 17:42:46 +0200

    Dodal razred `Inteligenca`

---
+++ CaptureGo/src/inteligenca/Inteligenca.java
@@ -0,0 +1,15 @@
+package inteligenca;
+
+import splosno.KdoIgra;
+
+public class Inteligenca extends KdoIgra{
+	
+	public Inteligenca(String ime) {
+		super(ime);
+	}
+	
+	public Inteligenca() {
+		this("Lana Ramšak, Jaka Vrhovnik");
+	}
+	
+}
