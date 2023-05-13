57ed136c5367c56dcacdac68762b00977ac964c0
Jaka Vrhovnik <jaka.vrhovnik2@gmail.com>
2023-05-14 00:14:49 +0200

    Uredil nekaj metod razreda `Vodja`

--- CaptureGo/src/Test.java
+++ CaptureGo/src/Test.java
@@ -1,17 +1,21 @@
 import gui.Okno;
 import inteligenca.Inteligenca;
 import logika.Igra;
+import vodja.Vodja;
 
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Testiram");
 		Okno okno = new Okno();
-		Igra igra = new Igra(9, 9);
-		okno.platno().nastaviIgro(igra);
+		// Igra igra = new Igra(9, 9);
+		// okno.platno().nastaviIgro(igra);
 		okno.pack();				// sestavi okno
 		okno.setVisible(true);		// pokaze okno
+		Vodja.okno = okno;
+		Vodja.ustvariNovoIgro();
 		
+		/*
 		Inteligenca random = new Inteligenca();
 		
 		for (int i = 0; i < 80; i++) {
@@ -25,6 +29,7 @@ public class Test {
 		}
 		
 		System.out.println(igra.steviloSvobodSkupine(igra.skupinaZetona(4, 4)));
+		*/
 	}
 
 }
