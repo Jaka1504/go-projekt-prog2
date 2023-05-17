b3cc231ce25f45a399a823cfe8c8b746dc3a247e
Lana Ramsak <ramsaklana@gmail.com>
2023-05-18 00:39:47 +0200

    Menu

--- CaptureGo/src/gui/Okno.java
+++ CaptureGo/src/gui/Okno.java
@@ -1,13 +1,18 @@
 package gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
 
+import vodja.Vodja;
+
 @SuppressWarnings("serial")
-public class Okno extends JFrame {
+public class Okno extends JFrame implements ActionListener {
 	
 	protected Platno platno;
 	
@@ -15,10 +20,41 @@ public class Okno extends JFrame {
 		super();
 		setTitle("Capture Go");
 		platno = new Platno(800, 800);	 	// ustvari v pomnilniku
-		add(platno);						// doda platno na okno
-		Menu meni= new Menu();
+		add(platno);				// doda platno na okno
+		
+		//menu
+		JMenuBar menu= new JMenuBar();
+		
+		JMenu menuIgra = new JMenu("Ustvari novo igro");	// naredi meni za novo igro
+	    JMenu menuGrafika = new JMenu("Grafika");	// naredi meni za lastnosti grafičnega vmesnika
 	    
-	    this.setJMenuBar(meni);  //doda meni na okno
+	    //sub-menu
+	    JMenu igraCloRac = new JMenu("Človek - Računalnik"); 
+	    JMenuItem igraClovekRacunalnik = new JMenuItem("Začne Človek");
+	    igraCloRac.add(igraClovekRacunalnik);
+	    igraClovekRacunalnik.addActionListener(this);
+	    JMenuItem igraRacunalnikClovek = new JMenuItem("Začne Računalnik");
+	    igraCloRac.add(igraRacunalnikClovek);
+	    igraClovekRacunalnik.addActionListener(this);
+	    menuIgra.add(igraCloRac);
+	    
+	    JMenuItem igraClovekClovek = new JMenuItem("Človek - Človek");
+	    menuIgra.add(igraClovekClovek);
+	    igraClovekClovek.addActionListener(this);
+	    JMenuItem igraRacunalnikRacunalnik = new JMenuItem("Računalnik - Računalnik");
+	    menuIgra.add(igraRacunalnikRacunalnik);
+	    igraRacunalnikRacunalnik.addActionListener(this);
+	    
+	    //grafika
+	    JMenuItem menuItem3 = new JMenuItem("Barve žetonov");
+	    menuGrafika.add(menuItem3);
+	    JMenuItem menuItem4 = new JMenuItem("...");
+	    menuGrafika.add(menuItem4);
+	    
+	    menu.add(menuIgra); 
+	    menu.add(menuGrafika);
+		
+	    this.setJMenuBar(menu);  //doda meni na okno
 	    
 		// da se program ustavi ko zapremo okno
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,5 +64,6 @@ public class Okno extends JFrame {
 	public Platno platno() {
 		return platno;
 	}
+	
 	
 }
